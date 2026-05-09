f4962207981ec77c533cf0557a4d0052ecd0366f
goncalo <goncalofcs18@gmail.com>
2026-05-10 00:06:52 +0100

    main update

--- backend/main.py
+++ backend/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+        "https://streakly-1.onrender.com/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
